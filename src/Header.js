45622e730c95851f44f94cbaaffae6e5175db259
abbas hassan <abbass.hassan.a7@gmail.com>
2023-11-20 01:23:57 +0200

    first commit

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className='header-contents'>
+        <h1 className='title'>Movies</h1>
+        <p>
+          Movies move us like nothing else can, whether they’re scary, funny, dramatic, romantic or anywhere in-between. So many titles, so much to experience.
+        </p>
+      </div>
+    </div>
+  );
+    
+}
+
+export default Header
---
+++ src/MoviesScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./MoviesScreen.css";
+import Nav from "./Nav";
+import Header from './Header';
+
+function MoviesScreen() {
+  return (
+    <div className="moviesScreen">
+      <Nav />
+      <Header />
+    </div>
+  );
+    
+}
+
+export default MoviesScreen
